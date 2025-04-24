932bbbd8506aa79249266f13de18e3806c11921f
Pedro Paulo <pedro.paulo60@aluno.ifce.edu.br>
2025-04-24 17:36:18 -0300

    TEST: Create initial tests for ExcelRepository class

---
+++ src/Tests/test_excel_repository.py
@@ -0,0 +1,22 @@
+from src.Core.ExcelRepository import ExcelRepository
+from pytest import fail
+
+def test_excel_repository_can_instantiate():
+    try:
+        database = ExcelRepository()
+    except Exception as e:
+        fail(f"instantiate fail: {e}")
+    else:
+        assert isinstance(database, ExcelRepository)
+
+def test_excel_repository_have_sheets():
+    database = ExcelRepository()
+    assert isinstance(database.sheets, dict)
+
+def test_can_add_sheet():
+    database = ExcelRepository()
+    assert database.add_sheet("test", "APTO")
+
+def test_can_delete_sheet():
+    database = ExcelRepository()
+    assert database.delete_sheet("APTO TEST")
