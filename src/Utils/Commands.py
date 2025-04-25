0b07ead363e11433b32792ad71f36b64c7701afa
Pedro Paulo <pedro.paulo60@aluno.ifce.edu.br>
2025-04-25 15:34:51 -0300

    REMOVE: Remove redundant modules

--- src/Utils/Commands.py
+++
@@ -1,8 +0,0 @@
-from os import system
-from time import sleep
-
-def clear():
-    system("cls")
-
-def wait(time:int):
-    sleep(time)
--- src/Utils/Prints.py
+++
@@ -1,5 +0,0 @@
-def print_apartments(apartments:list):
-    for apartment in apartments:
-        payday = round(apartment.payday)
-        rent = f"{round(apartment.rent)},00"
-        print(f"Apto. {apartment.number}|{apartment.condominium} - {apartment.tenant} - Dia de vencimento: {payday} / Valor: R& {rent}")
