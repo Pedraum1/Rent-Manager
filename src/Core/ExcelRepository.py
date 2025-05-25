f0e6edfcd2ee70078a0d044ac5aa936fbabceeb5
Pedro Paulo <pedro.paulo60@aluno.ifce.edu.br>
2025-05-25 14:52:39 -0300

    REFACTOR: Update import paths and enhance error handling in sheet management methods

--- src/Core/ExcelRepository.py
+++ src/Core/ExcelRepository.py
@@ -4,54 +4,57 @@ import pandas as pd
 if __name__ == "__main__":
     #Database Classes
     from DatabaseRepository import DatabaseRepository
-    from Models.Property import Property, PropertyDTO
+    from src.Models.Property import Property, PropertyDTO
 
     #Actions
-    from Actions.Database.AddSheet import AddSheet
-    from Actions.Database.DeleteSheet import DeleteSheet
+    from src.Actions.Database.AddSheet import AddSheet
+    from src.Actions.Database.DeleteSheet import DeleteSheet
+    from src.Actions.Database.EditSheet import EditSheet
 else:
     #Database Classes
     from .DatabaseRepository import DatabaseRepository
-    from ..Models.Property import Property, PropertyDTO
+    from src.Models.Property import Property, PropertyDTO
 
     #Actions
     from ..Actions.Database.AddSheet import AddSheet
     from ..Actions.Database.DeleteSheet import DeleteSheet
+    from ..Actions.Database.EditSheet import EditSheet
 
 class ExcelRepository(DatabaseRepository):
-    
     def __init__(self, table_file_name:str = "Table.xlsx"):
         current_directory = os.path.dirname(os.path.abspath(__file__))
         self.file_path = os.path.join(current_directory, "..", "Tables", table_file_name)
         self.connect_with_database()
         self._property_types = ['APTO', 'CASA', 'PTCM']
 
+    #public methods
     def connect_with_database(self):
-        self.sheets = dict()
+        sheets = dict()
         sheets_names = pd.ExcelFile(self.file_path, engine="openpyxl").sheet_names
 
         for sheet_name in sheets_names:
             pandas_sheet = pd.read_excel(self.file_path, sheet_name)
-            self.sheets[sheet_name] = pandas_sheet
-    
+            sheets[sheet_name] = pandas_sheet
+        self.sheets = sheets
+
     def get_overdue_rents(self, date:str):
-        #TODO: Create update_property function
+        #TODO: Create get_overdue_rents function
         pass
 
     def get_property_by_id(self):
-        #TODO: Create update_property function
+        #TODO: Create get_property_by_id function
         pass
 
     def get_all_properties(self) -> list:
-        #TODO: Create update_property function
+        #TODO: Create get_all_properties function
         pass
 
     def add_tenant(self, property_id, tenant_data) -> bool:
-        #TODO: Create update_property function
+        #TODO: Create add_tenant function
         pass
 
     def delete_tenant(self, property_id, tenant_data) -> bool:
-        #TODO: Create update_property function
+        #TODO: Create delete_tenant function
         pass
 
     def update_tenant(self, property_id, tenant_data) -> bool:
@@ -67,11 +70,32 @@ class ExcelRepository(DatabaseRepository):
         pass
 
     def add_sheet(self, sheet_name: str, type:str) -> bool:
-        return(AddSheet.handle(self, sheet_name, type))
-    
+        try:
+            AddSheet.handle(self, sheet_name, type)
+            self.connect_with_database()
+            return True
+        except Exception:
+            return False
+
     def delete_sheet(self, sheet_name: str)->bool:
-        return(DeleteSheet.handle(self, sheet_name))
-    
+        try:
+            DeleteSheet.handle(self, sheet_name)
+            self.connect_with_database()
+            return True
+        except Exception:
+            return False
+
+    def edit_sheet(self, sheet_name: str, new_name:str = None, type:str = None) -> bool:
+        try:
+            EditSheet.handle(self, sheet_name, new_name, type)
+            self.connect_with_database()
+            return True
+        except Exception:
+            return False
+
+
+    #private methods
+
     def _verify_property_type(self, property_type:str) -> bool:
         return property_type in self._property_types
     
@@ -81,5 +105,7 @@ class ExcelRepository(DatabaseRepository):
 if __name__ == "__main__":
     excel = ExcelRepository()
 
-    excel.add_sheet("test", "APTO")
-    excel.delete_sheet("APTO TEST")
+    add = excel.add_sheet("test 3", "APTO")
+    result = excel.edit_sheet("APTO TEST 3", type= "APTO")
+    delete = excel.delete_sheet("APTO TEST 3")
+    print(f"{add} {result} {delete}")
